6b5d2b17f832667ebc0a6cde5a144b7b6042e032
Furkan Cingöz <efcingoz@outlook.com>
2023-09-08 22:42:12 +0300

    Initial Commit

---
+++ catchTheKennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  catchTheKennyGame
+//
+//  Created by Furkan Cingöz on 8.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
